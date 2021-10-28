8ce7d99f83ffcebdb73e29feaa31876f287b9377
cha-suyeon <suyeon.chaa@gmail.com>
2021-10-28 14:21:14 +0900

    upload ch6

--- Ch6_Stack/section03/10_code06-08_수정.py
+++ Ch6_Stack/section03/10_code06-08_수정.py
@@ -65,7 +65,7 @@ top = -1
 if __name__ == "__main__":
     select = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택==>")
 
-    while (select != 'X' and select != 'x'):
+    while True:
 
         if select == 'I' or select == 'i':
             data = input('입력할 데이터==>')
@@ -81,10 +81,13 @@ if __name__ == "__main__":
             data = peek()
             print('확인된 데이터==>', data)
             print('스택 상태: ', stack)
+        
+        elif select != 'X' or select != 'x':
+            break;
 
         else:
             print('입력이 잘못됨')
-
+        
         select = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택==>")
 
     print('프로그램 종료!')
