65b22723d5313a175b028aa01ffe8b330a0cd136
cha-suyeon <suyeon.chaa@gmail.com>
2021-11-04 17:02:30 +0900

    upload ch7 sec02

---
+++ Ch7_Queue/section02/1.py
@@ -0,0 +1,11 @@
+# Section 02
+# 1. 큐 생성
+
+# 큐는 배열 형태로 구현할 수 있다.
+# 큐의 머리와 꼬리를 가리키는 front와 rear는 -1로 초기화
+
+
+queue = [None, None, None, None, None]
+front = rear = -1
+print(queue)
+print(front, rear)
