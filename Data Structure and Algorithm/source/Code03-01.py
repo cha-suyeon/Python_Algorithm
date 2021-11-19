83abd55663ed136adefd6b3d2cd60f839e3fa28a
cha-suyeon <suyeon.chaa@gmail.com>
2021-11-20 00:55:48 +0900

    upload ch3

---
+++ Data Structure and Algorithm/source/Code03-01.py
@@ -0,0 +1,15 @@
+katok = []          # 빈 배열
+
+def add_data(friend) :
+        
+	katok.append(None)
+	kLen = len(katok)
+	katok[kLen-1] = friend
+
+add_data('다현')
+add_data('정연')
+add_data('쯔위')
+add_data('사나')
+add_data('지효')
+
+print(katok)
