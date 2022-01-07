5969b07d887dc4422bdad983fc7bfde146d94009
cha-suyeon <suyeon.chaa@gmail.com>
2022-01-08 01:25:12 +0900

    upload ch6

---
+++ Data Structure and Algorithm/source/Self06-01.py
@@ -0,0 +1,17 @@
+def push(data) :
+    global SIZE, stack, top
+    if (top >= SIZE - 1):
+        print("스택이 꽉 찼습니다.")
+        return
+
+    top += 1
+    stack[top] = data
+
+SIZE = 5
+stack = ["커피", "녹차", "꿀물", "콜라", None]
+top = 3
+
+print(stack)
+push("환타")
+print(stack)
+push("게토레이")
---
+++ Data Structure and Algorithm/source/Self06-02.py
@@ -0,0 +1,19 @@
+def pop() :
+    global SIZE, stack, top
+    if (top == -1) :
+        print("스택이 비었습니다.")
+        return
+    data = stack[top]
+    stack[top] = None
+    top -= 1
+    return data
+
+SIZE = 5
+stack = ["커피", None, None, None, None]
+top = 0
+
+print(stack)
+retData = pop()
+print("추출한 데이터 -->", retData)
+print(stack)
+retData = pop()
