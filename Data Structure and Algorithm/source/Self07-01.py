9d295a9861e8a142ce4fa474f0a6a24bd795f12c
cha-suyeon <suyeon.chaa@gmail.com>
2022-01-09 19:49:49 +0900

    upload ch7

---
+++ Data Structure and Algorithm/source/Self07-01.py
@@ -0,0 +1,17 @@
+def enQueue(data) :
+    global SIZE, queue, front, rear
+    if (rear == SIZE-1) :
+        print("큐가 꽉 찼습니다.")
+        return
+    rear += 1
+    queue[rear] = data
+
+SIZE = 5
+queue = ["화사", "솔라", "문별", "휘인", None]
+front = -1
+rear = 3
+
+print(queue)
+enQueue("선미")
+print(queue)
+enQueue("재남")
---
+++ Data Structure and Algorithm/source/Self07-02.py
@@ -0,0 +1,20 @@
+def deQueue() :
+    global SIZE, queue, front, rear
+    if (front == rear) :
+        print("큐가 비었습니다.")
+        return None
+    front += 1
+    data = queue[front]
+    queue[front] = None
+    return data
+
+SIZE = 5
+queue = ["화사", None, None, None, None]
+front = -1
+rear = 0
+
+print(queue)
+retData = deQueue()
+print("추출한 데이터 -->", retData)
+print(queue)
+retData = deQueue()
