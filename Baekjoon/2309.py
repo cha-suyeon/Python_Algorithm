03c5613122d58376d7c79688362ad4bce1cb1db5
cha-suyeon <suyeon.chaa@gmail.com>
2024-10-12 16:03:27 +0900

    1010

--- Baekjoon/2309.py
+++ Baekjoon/2309.py
@@ -1,3 +1,24 @@
+# 수정
+from itertools import combinations
+
+key = []
+for i in range(9):
+    key.append(int(input()))
+
+max_sum = 0
+result = []
+M = 100
+
+for k in combinations(key, 7):
+    key_sum = sum(k)
+    if key_sum == M:
+        result = list(k)
+
+result.sort()
+for num in result:
+    print(num)
+
+# 처음 푼 코드
 from itertools import combinations
 
 key = []
