0e059552726a54c5005f84fd53598f6585b342f4
cha-suyeon <suyeon.chaa@gmail.com>
2025-09-22 17:30:38 +0900

    leetcode

---
+++ leetcode/number-of-islands.py
@@ -0,0 +1,5 @@
+from typing import List
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
---
+++ leetcode/shortest-path-in-binary-matrix.py
@@ -0,0 +1,5 @@
+from typing import List
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        pass
