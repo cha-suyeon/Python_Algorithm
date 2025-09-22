27fdff8daaf5955e4d273192d912b65b6c7f05e3
suyeon <suyeon@suyeonui-MacBookPro.local>
2025-09-22 22:52:42 +0900

    Leetcode: bfs/dfs

--- leetcode/number-of-islands.py
+++ leetcode/number-of-islands.py
@@ -2,4 +2,32 @@ from typing import List
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
         
+        row_len = len(grid)
+        col_len = len(grid[0])
+        visited = [[False] * col_len for _ in row_len]
+        
+        dr = [-1, 1, 0, 0]
+        dc = [0, 0, -1, 1]
+        
+        def dfs(r, c):
+            visited[r][c] = True
+            
+            for i in range(4):
+                nr = r + dr[i]
+                nc = c + dc[i]
+                
+                if 0 <= nr < row_len and 0 <= nc < col_len:
+                    if grid[nr][nc] == '1' and not visited[nr][nc]:
+                        dfs(nr, nc)
+                        
+        island_count = 0
+        for r in range(row_len):
+            for c in range(col_len):
+                if grid[r][c] == '1' and not visited[r][c]:
+                    island_count += 1
+                    dfs(r, c)
+                    
+        return island_count
--- leetcode/shortest-path-in-binary-matrix.py
+++ leetcode/shortest-path-in-binary-matrix.py
@@ -1,5 +1,35 @@
 from typing import List
+from collections import deque
 
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        pass
+        shortest_dist = -1
+        row_len = len(grid)
+        col_len = len(grid[0])
+        
+        if grid[0][0] == 1 or grid[row_len -1][col_len - 1] == 1:
+            return shortest_dist
+        
+        visited = [[False] * col_len for _ in range(row_len)]
+        dr = [0, 1, 1, 1, 0, -1, -1, -1]
+        dc = [1, 1, 0, -1, -1, -1, 0, 1]
+        
+        queue = deque()
+        queue.append((0, 0, 1))
+        visited[0][0] = True
+        
+        while queue:
+            cur_r, cur_c, cur_dist = queue.popleft()
+            if cur_r == row_len -1 and cur_c == col_len - 1:
+                shortest_dist = cur_dist
+                break
+            for i in range(8):
+                next_r = cur_r + dr[i]
+                next_c = cur_c + dc[i]
+                if (0 <= next_r < row_len) and (0 <= next_c < col_len):
+                    if grid[next_r][next_c] == 0:
+                        if not visited[next_r][next_c]:
+                            queue.append((next_r, next_c, cur_dist + 1))
+                            visited[next_r][next_c] = True
+                            
+        return shortest_dist
